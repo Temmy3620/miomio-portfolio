70deee345fd20681e722ee2a5cbb5ba29203b88a
terasakimio <me512papiko512@gmail.com>
2024-10-10 16:29:38 +0700

    first commit

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+const Test = () => {
+  return (
+    <div>
+      <header>
+        <h1>Mio – Freelance Web Developer</h1>
+        <p>Next.js、React、TypeScriptを用いたWebアプリケーション開発を専門にしているフリーランスのエンジニアです。フロントエンドからバックエンドまで幅広く対応しています。</p>
+      </header>
+    </div>
+  );
+}
+
+export default Test;
