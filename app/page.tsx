98d02e843018ac42ee65d94ba1f126788aef5733
terasakimio <me512papiko512@gmail.com>
2024-10-16 20:45:37 +0700

    SNSリンクをクリックしたら別タブで飛べるようにする

--- app/page.tsx
+++ app/page.tsx
@@ -59,6 +59,7 @@ export default function Home() {
         <ul role="list">
           <li className="flex">
             <a
+              target="_blank"
               className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
               href="https://x.com/kbBoNj3eiRW4fvD"
             >
@@ -74,6 +75,7 @@ export default function Home() {
           </li>
           <li className="mt-4 flex">
             <a
+              target="_blank"
               className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
               href="https://www.instagram.com/temmy_insta"
             >
@@ -90,6 +92,7 @@ export default function Home() {
           </li>
           <li className="mt-4 flex">
             <a
+              target="_blank"
               className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
               href="https://github.com/Temmy3620"
             >
@@ -109,6 +112,7 @@ export default function Home() {
           </li>
           <li className="mt-4 flex">
             <a
+              target="_blank"
               className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
               href="https://www.linkedin.com/in/%E6%9C%AA%E7%B7%92-%E5%AF%BA%E5%B4%8E-25b6732b6"
             >
@@ -124,6 +128,7 @@ export default function Home() {
           </li>
           <li className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40 flex">
             <a
+              target="_blank"
               className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
               href="mailto:me512papiko512@gmail.com"
             >
