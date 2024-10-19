ad2d1eb6b982d3e0fb354f68af815d3a440536c3
terasakimio <me512papiko512@gmail.com>
2024-10-20 00:57:11 +0700

    履歴修正

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,22 +1,25 @@
 const articles = [
   {
-    title: 'Crafting a design system for a multiplanetary future',
-    href: '/articles/crafting-a-design-system-for-a-multiplanetary-future',
-    date: '2022-09-05',
-    description:
-      'Most companies try to stay ahead of the curve when it comes to visual design, but for Planetaria we needed to create a brand that would still inspire us 100 years from now when humanity has spread across our entire solar system.',
+    title: 'I’m starting my own business and beginning my career as a freelance engineer',
+    date: '2024-09-30'
   },
   {
-    title: 'Introducing Animaginary: High performance web animations',
-    href: '/articles/introducing-animaginary',
-    date: '2022-09-02',
-    description:
-      'When you’re building a website for a company as ambitious as Planetaria, you need to make an impression. I wanted people to visit our website and see animations that looked more realistic than reality itself.',
+    title: 'I have been contracted by Lograph Inc. for outsourced work.',
+    date: '2024-10-01',
   },
   {
-    title: 'Rewriting the cosmOS kernel in Rust',
-    href: '/articles/rewriting-the-cosmos-kernel-in-rust',
-    date: '2022-07-14',
+    title: 'Lograph Inc: Start implementing the Offline Conversion API using the Microsoft Bing API.',
+    date: '2024-10-01',
+    description:
+      'When you’re building a website for a company as ambitious as Planetaria, you need to make an impression. I wanted people to visit our website and see animations that looked more realistic than reality itself',
+    link: {
+      label: 'Detail Product',
+      href: '/articles/crafting-a-design-system-for-a-multiplanetary-future'
+    },
+  },
+  {
+    title: 'Lograph Inc: End implementing the Offline Conversion API using the Microsoft Bing API',
+    date: '2022-10-30',
     description:
       'When we released the first version of cosmOS last year, it was written in Go. Go is a wonderful programming language, but it’s been a while since I’ve seen an article on the front page of Hacker News about rewriting some important tool in Go and I see articles on there about rewriting things in Rust every single week.',
   },
@@ -35,16 +38,14 @@ export default function Experience() {
       </header>
       <div className="mt-16 sm:mt-20">
         <div className="md:border-l md:pl-6 md:border-zinc-700/40">
-          <div className="flex max-w-3xl flex-col space-y-16">
+          <div className="flex max-w-3xl flex-col space-y-8">
             {articles.map((article) => (
               <article key={article.title} className="md:grid md:grid-cols-4 md:items-baseline">
                 <div className="md:col-span-3 group relative flex flex-col items-start">
                   <h2 className="text-base font-semibold tracking-tight text-zinc-100">
-                    <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl bg-zinc-800/50"></div>
-                    <a href={article.href}>
-                      <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                      <span className="relative z-10">{article.title}</span>
-                    </a>
+                    <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 opacity-0 sm:-inset-x-6 sm:rounded-2xl bg-zinc-800/50"></div>
+                    <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+                    <span className="relative z-10">{article.title}</span>
                   </h2>
                   <time className="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-zinc-500 pl-3.5" dateTime={article.date}>
                     <span className="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
@@ -52,17 +53,23 @@ export default function Experience() {
                     </span>
                     {new Date(article.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
                   </time>
-                  <p className="relative z-10 mt-2 text-sm text-zinc-400">
-                    {article.description}
-                  </p>
-                  <div aria-hidden="true" className="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">
-                    Read article
-                    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" className="ml-1 h-4 w-4 stroke-current">
-                      <path d="M6.75 5.75 9.25 8l-2.5 2.25" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-                    </svg>
-                  </div>
+                  {article.description && (
+                    <p className="relative z-10 mt-2 text-sm text-zinc-400">
+                      {article.description}
+                    </p>
+                  )}
+                  {article.link?.href && (
+                    <a href="#" className="hover:opacity-100" target="_blank" rel="noopener noreferrer">
+                      <div aria-hidden="true" className="relative z-10 mt-2 flex items-center text-sm font-medium text-teal-500">
+                        {article.link.label}
+                        <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" className="ml-1 h-4 w-4 stroke-current">
+                          <path d="M6.75 5.75 9.25 8l-2.5 2.25" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                        </svg>
+                      </div>
+                    </a>
+                  )}
                 </div>
-                <time className="mt-1 hidden md:block relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500" dateTime={article.date}>
+                <time className="mt-1 hidden md:block relative z-10 order-first mb-3 flex items-center text-sm text-zinc-500" dateTime={article.date}>
                   {new Date(article.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
                 </time>
               </article>
