9b9b434774c8ea22d1c22a9d880a1340842264d8
terasakimio <me512papiko512@gmail.com>
2024-10-20 17:10:51 +0700

    projectのテキストカラーproject事に設定できるよう変更

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,16 @@
+import test from "node:test";
+
 const projects = [
   {
     name: 'Microsoft Bing API',
+    textColor: 'text-blue-700',
     href: 'http://planetaria.tech',
-    imageSrc: '/images/Sample.png',
+    imageSrc: 'https://media.licdn.com/dms/image/D4E12AQG-VLYWTLqTBw/article-cover_image-shrink_720_1280/0/1723114682752?e=2147483647&v=beta&t=fvzCJ8dOVMSS1TV-xYkkUvBQ_XNP3qqTY6fw56T9494',
   },
   {
     name: 'Sample',
     href: '#',
-    imageSrc: '/images/Sample.png',
+    imageSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDolSgvVOH3Uje7JydOPsaCGqKFXad5f85Sg&s',
   },
   {
     name: 'Sample',
@@ -52,54 +55,20 @@ export default function Projects() {
           {projects.map((project) => (
             <li className="group relative flex flex-col items-start">
               <a href="#" className="group">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-2xl bg-gray-200 xl:aspect-h-8 xl:aspect-w-7 relative">
+                <div className="md:w-75 md:h-47 overflow-hidden rounded-2xl bg-gray-200 relative">
                   <img
                     src={project.imageSrc}
                     alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-                    className="h-full w-full object-cover object-center opacity-40 transition-opacity duration-250 group-hover:opacity-75"
+                    className="h-full w-full object-contain object-center opacity-40 transition-opacity duration-250 group-hover:opacity-75"
                   />
                   <div className="absolute inset-0 flex items-end justify-start transition-opacity duration-250 opacity-100 group-hover:opacity-10">
-                    <p className="text-gray-800 text-xl font-bold p-3">{project.name}</p>
+                    <p className={`text-xl font-bold p-3 ${project.textColor}`}>{project.name}</p>
                   </div>
                 </div>
               </a>
             </li>
           ))}
         </ul>
-        {/*
-        <ul role="list" className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3">
-          {projects.map((project) => (
-            <li key={project.name} className="group relative flex flex-col items-start">
-              <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-                <img
-                  alt={project.name}
-                  src={project.imageSrc}
-                  className="h-8 w-8"
-                />
-              </div>
-              <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-                <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
-                <a href={project.link.href}>
-                  <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                  <span className="relative z-10">{project.name}</span>
-                </a>
-              </h2>
-              <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-                {project.description}
-              </p>
-              <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-                <svg viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6 flex-none">
-                  <path
-                    d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
-                <span className="ml-2">{project.link.label}</span>
-              </p>
-            </li>
-          ))}
-        </ul>
-      */}
       </div>
     </div>
   );
