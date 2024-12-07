b6f560ad8c0f9542680664b90333fee69999720b
terasakimio <me512papiko512@gmail.com>
2024-12-07 15:42:36 +0800

    経歴の題名変更

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -31,7 +31,7 @@ export default function Experience() {
     <div className="mx-auto max-w-2xl lg:max-w-5xl">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight sm:text-5xl text-zinc-100">
-          Writing on software design, company building, and the aerospace industry.
+          Writing about the projects I have been involved in, freelance work, and my career history
         </h1>
         <p className="mt-6 text-base text-zinc-400">
           All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.
