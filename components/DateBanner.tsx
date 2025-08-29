ba6bef342af313bcc6933b48b73298538f1b924b
terasakimio <me512papiko512@gmail.com>
2025-08-29 18:47:19 +0900

    外細かい部分調整2

--- components/DateBanner.tsx
+++ components/DateBanner.tsx
@@ -14,11 +14,8 @@ export function DateBanner({ date = new Date(), className = "" }: DateBannerProp
 
   return (
     <div className={`relative inline-block ${className}`}>
-      <div className="bg-zinc-100 text-zinc-800 dark:bg-zinc-800/90 dark:text-zinc-200 py-1 pl-3 pr-7 shadow-lg backdrop-blur border-t border-b border-zinc-900/10 dark:border-white/5 relative">
+      <div className="bg-zinc-100 text-zinc-800 dark:bg-zinc-800/90 dark:text-zinc-200 py-1 pl-3 pr-3 shadow-lg backdrop-blur border-t border-b border-zinc-900/10 dark:border-white/5 relative">
         {formattedDate}
-        <div className="absolute top-0 right-0 h-full w-6 overflow-hidden">
-          <div className="absolute rotate-45 bg-white dark:bg-zinc-900 h-8 w-8 -right-6 top-1/2 -translate-y-1/2"></div>
-        </div>
       </div>
     </div>
   )
